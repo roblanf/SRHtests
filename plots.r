056884ca561db9c84daa88cb69eb43f0089d0c2b
roblanf <rob.lanfear@gmail.com>
2017-05-03 15:54:28 +1000

    new plots

--- plots.r
+++ plots.r
@@ -2,47 +2,64 @@ library(ggplot2)
 library(plyr)
 
 
-proportion_p <- function(pvals){
-  
-  # ignore NAs
-  pvals = pvals[complete.cases(pvals)]
-  
-  if(length(pvals)>9){
-    sig = sum(pvals<0.05)
-    return(sig / length(pvals))
-  }else{
-    return(NA)
-  }
+sig_p <- function(pvals){
+	pvals = pvals[complete.cases(pvals)]
+	return(sum(pvals<0.05))
 }
 
-d = read.csv("~/Desktop/test.csv")
+nonsig_p <- function(pvals){
+	pvals = pvals[complete.cases(pvals)]
+	return(sum(pvals>=0.05))
+}
+
+na_p <- function(pvals){
+	return(sum(is.na(pvals)))
+}
+
+binom_p <- function(sig, nonsig){
+	total = sig + nonsig
+	binom.test(sig, total, p = 0.05, alternative = 'greater')
+}
+
+#read in the data
+d = read.csv("Documents/github/SRHtests/ouput.csv")
 d$p.value = as.numeric(as.character(d$p.value)) # Oh R
 
-# this shows the proportion of p values <0.2 for each charset (where there were at least 10 p values)
-d_prop = ddply(d, .(Dataset, Charset, Test), summarise, proportion = proportion_p(p.value))
-p = ggplot(d_prop, aes(x = proportion, group = Test))
-p + geom_histogram() + geom_vline(xintercept = 0.05, colour='red', alpha = 0.5) + facet_wrap(~Dataset, scales = 'free_y')
+# make a dataframe with one row per charset, p values that are <0.05, >=0.05, NA, binomial
+charsets = ddply(d, .(Dataset, Charset, Test), summarise, sig = sig_p(p.value), non = nonsig_p(p.value), na = na_p(p.value))
+charsets$binomial = apply(charsets, 1, function(x)  binom.test(c(as.integer(x[4]), as.integer(x[5])), p = 0.05, alternative = 'greater')$p.value)
+
+
+
 
 # we can summarise this further as a simple number of pass / fail charsets for each alignment
-pass <- function(proportions){ proportions = proportions[complete.cases(proportions)]; return(sum(proportions<=0.05))}
-fail <- function(proportions){ proportions = proportions[complete.cases(proportions)]; return(sum(proportions>0.05))}
-na <- function(proportions){ return(sum(is.na(proportions)))}
-d_passfail = ddply(d_prop, .(Dataset), summarise, fail = fail(proportion), pass = pass(proportion), no_data = na(proportion))
+pass <- function(pvals){ return(sum(pvals>=0.05))}
+fail <- function(pvals){ return(sum(pvals<0.05))}
+passfail = ddply(charsets, .(Dataset), summarise, fail = fail(binomial), pass = pass(binomial))
+
+
 
 # we can look at an individual dataset / charset like this
 dnum=6
 name = levels(d$Dataset)[dnum]
-propsub = subset(d_prop, Dataset==name)
+propsub = subset(charsets, Dataset==name)
 
 # worst charset in the dataset
-cs = propsub$Charset[which(propsub$proportion==max(propsub$proportion, na.rm=T))][1]
+cs = propsub$Charset[which(propsub$binomial==min(propsub$binomial, na.rm=T))][1]
 c = ggplot(subset(d, Dataset==name & Charset==as.character(cs)), aes(x = p.value))
 c + geom_histogram() + ggtitle(paste(cs, 'from', name))
 
 
-# whole dataset
-p = ggplot(subset(d, Dataset==levels(d$Dataset)[dnum]), aes(x=p.value, colour = Charset))
+# plot a whole dataset (gets tricky with lots of lines...)
+p = ggplot(subset(d, Dataset==levels(d$Dataset)[dnum]), aes(x=p.value))
 
-p + geom_histogram(aes(fill=Charset)) + facet_wrap(~Charset)
-p + geom_histogram(aes(fill=Charset)) + facet_grid(Charset~Test, scales='free_y')
+p + geom_histogram() + facet_wrap(~Charset)
+p + geom_histogram() + facet_grid(Charset~Test, scales='free_y')
+p + geom_line(aes(color=Charset), stat="density", size=1, alpha=0.4)
 
+# for lots of charsets
+
+
+# here's a bad dataset
+p = ggplot(subset(d, Dataset=="Borowiec_2015"), aes(x=p.value))
+p + geom_line(stat="density", alpha=0.25, aes(group=Charset, y = ..scaled..))
