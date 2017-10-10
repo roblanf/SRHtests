fdc0370a5c95e7670101fa6b5ab30edc14323baa
roblanf <rob.lanfear@gmail.com>
2017-10-11 10:18:36 +1100

    update figure 1

--- figure1.r
+++ figure1.r
@@ -6,4 +6,4 @@ d = d[,-c(1,2, 10, 11, 12)]
 names(d) = c("charset", "test", "fail", "pass", "NA", "p_binomial", "dataset", "sites")
 
 # overall p values from binomial tests
-ggplot(d, aes(x=p_binomial)) + geom_histogram() + facet_wrap(~test, ncol=1)
+ggplot(d, aes(x=p_binomial)) + geom_histogram(bins=40) + facet_wrap(~test, ncol=1) + geom_vline(xintercept = 0.05, colour = 'red')
