edf35951662dae810e61b8c90d66a22af326a109
roblanf <rob.lanfear@gmail.com>
2017-11-29 08:05:03 +1100

    update figures

--- figure2.r
+++ figure2.r
@@ -1,6 +1,19 @@
 library(ggplot2)
 
-d = read.csv("~/Dropbox/Projects_Current/systematic_bias/tables/table2.csv")
+d = read.csv("~/Dropbox/Projects_Current/systematic_bias/tables/summary_charsets.csv")
 
-# overall p values from binomial tests
-ggplot(d, aes(Dataset)) + geom_bar(aes(weight = Bad_Charsets..)) + facet_wrap(~Test, ncol=1) + theme(axis.text.x=element_text(angle=90,hjust=1))
+
+
+# is the number of bad subsets linked to the 
+# we should really do this with tree lengths...
+ggplot(d, aes(x = Avg.Bad.Charset.length., y = Avg.Not.bad.Charset.length.)) + geom_point()
+
+ggplot(d, aes(x = Delta.length..Bad.NB.)) + geom_histogram() + xlim(-100, 100)
+
+ggplot(d, aes(x = Delta.length..Bad.NB.)) + geom_density() + xlim(-100, 100)
+
+
+# sign test
+diffs = d$Delta.length..Bad.NB.
+pos = sum(diffs>0, na.rm = T)
+neg = sum(diffs<0, na.rm = T)
