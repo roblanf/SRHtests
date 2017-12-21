ffe8cf5b88e903fa8426e1dbf7208ec538897db1
roblanf <rob.lanfear@gmail.com>
2017-12-21 12:17:09 +1100

    update figure 1

--- figure1.r
+++ figure1.r
@@ -9,7 +9,6 @@ names(d) = c("charset", "test", "fail", "pass", "undetermined", "p_binomial", "d
 # we only count charsets where we could measure at least 20 pairs of sequences for a test
 d$p = d$p_binomial
 d$N = d$fail+d$pass
-d$p[which(d$N<20)] = NA
 
 # now we can categorise pass/fail/NA for the p-values
 d$result = "undetermined"
@@ -21,14 +20,6 @@ d$result = addNA(d$result)
 d$result = factor(d$result, levels = c("pass", "undetermined", "fail"))
 
 
-
-# Figure 1A overall p values from binomial tests
-ggplot(d, aes(x=p)) + 
-    geom_histogram(bins=40) + 
-    facet_wrap(~test, ncol=1) + 
-    geom_vline(xintercept = 0.05, colour = 'red', linetype = 'dashed') +
-    facet_wrap(~test, ncol=1)
-
 # dataset-by-test how many are pass/fail/NA
 gg_color_hue <- function(n) {
     hues = seq(15, 375, length=n+1)
@@ -36,14 +27,13 @@ gg_color_hue <- function(n) {
 }
 mycols <- gg_color_hue(length(unique(d$result)))
 cols = mycols
-cols[2] = 'grey'
+cols[2] = 'black'
 cols[1] = mycols[3]
-cols[3] = mycols[1]
 
-# Figure 1B - proportion of each dataset that fails...
+# Figure 1 - proportion of each dataset that fails...
 ggplot(d, aes(x = dataset)) + 
     geom_bar(aes(fill = result), position = "fill", na.rm = T) + 
     facet_wrap(~test, ncol=1) + 
     scale_fill_manual(values=cols) + 
     ylab("Proportion of character sets") +
-    theme(axis.text.x=element_text(angle=90,hjust=1))
+    theme(axis.text.x=element_text(angle=90,hjust=1)) + theme(legend.position="none")
